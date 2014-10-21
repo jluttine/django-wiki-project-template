56320f3e3f03c175a21c337755d82e1de24768d1
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-10-21 20:32:45 +0300

    Localization to Finland

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -64,8 +64,8 @@ DATABASES = {
 }
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'fi-fi'
+TIME_ZONE = 'Europe/Helsinki'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
