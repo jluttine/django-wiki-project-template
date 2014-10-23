20cc2e84eb1b933545da9480e6c10c3e593a177c
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2014-10-23 09:59:38 +0300

    Update from django_notify to django_nyt

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,25 +1,27 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.contrib import admin
-
-from wiki.urls import get_pattern as get_wiki_pattern
-from django_notify.urls import get_pattern as get_notify_pattern
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
-# Following is the url configuration for django-wiki. It puts the wiki in / so
-# it’s important that it is the last entry in urlpatterns. You can also put it
-# in /wiki by putting '^wiki/' as the pattern.
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
 
+from wiki.urls import get_pattern as get_wiki_pattern
+from django_nyt.urls import get_pattern as get_notify_pattern
 urlpatterns += patterns('',
-    url(r'^notify/', get_notify_pattern()),
-    url(r'', get_wiki_pattern())
+    (r'^notify/', get_notify_pattern()),
+    (r'', get_wiki_pattern())
 )
